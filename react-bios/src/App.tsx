8083793ec81b4d732817b4cbb627d1645c4eb946
David Breckx <david.breckx@telenet.be>
2025-10-27 17:29:20 +0100

    Einde les 6

--- react-bios/src/App.tsx
+++ react-bios/src/App.tsx
@@ -16,22 +16,22 @@ interface MovieResponse {
   total_results: number;
 }
 
-interface Movie {
-  adult: boolean;
-  backdrop_path: string;
-  genre_ids: number[];
-  id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  release_date: string;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-}
+// interface Movie {
+//   adult: boolean;
+//   backdrop_path: string;
+//   genre_ids: number[];
+//   id: number;
+//   original_language: string;
+//   original_title: string;
+//   overview: string;
+//   popularity: number;
+//   poster_path: string;
+//   release_date: string;
+//   title: string;
+//   video: boolean;
+//   vote_average: number;
+//   vote_count: number;
+// }
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -58,7 +58,7 @@ function App() {
     try {
       const response = await fetch(BASE_URL, {
         headers: {
-          Authorization: API_TOKEN,
+          Authorization: import.meta.env.VITE_TMDB_API_KEY,
         },
       });
       const data = (await response.json()) as MovieResponse;
--- react-bios/src/main.tsx
+++ react-bios/src/main.tsx
@@ -5,21 +5,28 @@ import App from "./App.tsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import DetailsPage from "./pages/DetailsPage.tsx";
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.tsx";
+import RootLayout from "./layouts/RootLayout.tsx";
 
 const browserRouter = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "details/:movieId",
-    element: <DetailsPage />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "details/:movieId",
+        element: <DetailsPage />,
+      },
+    ],
   },
 ]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={browserRouter} />
-    {/* <App /> */}
+    <RouterProvider router={browserRouter} />\{/* <App /> */}
   </StrictMode>
 );
